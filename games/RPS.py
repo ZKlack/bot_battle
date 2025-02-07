aa226dcecd11ef83b294f0c49b55dd74aa00c0f0
ZKlack <xklack@outlook.sa>
2025-02-07 16:50:04 +0300

    RPS done!

--- games/RPS.py
+++ games/RPS.py
@@ -1,14 +1,87 @@
 import zudge
-import sys
+from sys import argv
 
 #file managment
 
-if len(sys.argv)<3:
-    print("Usage: python RPS.py <player1> <player2> [<output file>] [<number of matches>]")
+if len(argv)<3:
+    print("Usage: python RPS.py <player1> <player2> [<output file>] [<number of rounds>]")
     exit(1)
-p = zudge.start(sys.argv[1:3])
+p = zudge.start(argv[1:3])
 
-if len(sys.argv)>=4:
-    zudge.open(sys.argv[3])
+if len(argv)>=4:
+    zudge.open(argv[3])
 
-#GAME!!!
+rounds: int = 10
+if len(argv)>=5:
+    rounds = int(argv[4])
+
+#game parts
+
+score: list[int] = [0,0]
+move: list[str] = ["",""]
+moves: list[str] = ["R","P","S"]
+beats: dict[str,str] = {
+    "R":"P",
+    "P":"S",
+    "S":"R"
+}
+
+zudge.print(f"p0: {argv[1]}")
+zudge.print(f"p1: {argv[2]}")
+
+def disqualify(i:int):
+    if i==2:
+        zudge.print("BOTH DISUALIFIED")
+        finalize()
+    zudge.print(f"p{i} DISQUALIFIED")
+    score[i] += score[1-i]
+    finalize()
+
+def finalize():
+    zudge.print("\n\n")
+    zudge.print(f"p0: {score[0]}\t{argv[1]}")
+    zudge.print(f"p1: {score[1]}\t{argv[2]}")
+    if score[0]==score[1]:
+        zudge.print("DRAW")
+    else:
+        zudge.print(argv[1 if score[0]>score[1] else 2])
+    exit(0)
+
+def ask():
+    move[0] = zudge.read(p[0])
+    move[1] = zudge.read(p[1])
+    if move[0] is None and move[1] is None:
+        disqualify(2)
+    if move[0] is None:
+        disqualify(0)
+    if move[1] is None:
+        disqualify(1)
+
+def judge():
+    if (move[0] not in moves) and (move[1] not in moves):
+        disqualify(2)
+    if move[0] not in moves:
+        disqualify(0)
+    if move[1] not in moves:
+        disqualify(1)
+    if move[0] == move[1]:
+        zudge.print("dr"," ")
+    elif move[0] == beats[move[1]]:
+        zudge.print("p0"," ")
+        score[0] += 1
+    elif move[1] == beats[move[0]]:
+        zudge.print("p1"," ")
+        score[1] += 1
+
+def tell():
+    zudge.write(p[0],move[1])
+    zudge.write(p[1],move[0])
+
+#game
+
+for _ in range(rounds):
+    ask()
+    judge()
+    tell()
+
+finalize()
