0d0a7fc618bdd284cbd2996636676d1318c4980c
ZKlack <xklack@outlook.sa>
2025-02-07 10:37:23 +0300

    updated RPS.py to comply with zudge,py changes

--- games/RPS.py
+++ games/RPS.py
@@ -1,45 +1,31 @@
 import zudge
+import sys
 
-#open output file if provided
-FILE = None
-if len(zudge.sys.argv) >= 4:
-    FILE = open(zudge.sys.argv[3],mode="w")
-    zudge.atexit.register(lambda: FILE.close())
+#file managment
 
-#write function to handle if the output is on the terminal or a file
-def write(txt:str, end:str="\n"):
-    if FILE is not None:
-        return FILE.write(txt+end)
-    print(txt,end=end)
-    return 0
-
-#check that players are online
-if zudge.p1.poll() is not None:
-    print("p1 is offline")
+if len(sys.argv)<3:
+    print("Usage: python RPS.py <player1> <player2> [<output file>]")
     exit(1)
-if zudge.p2.poll() is not None:
-    print("p2 is offline")
-    exit(1)
+zudge.start(sys.argv[1:3])
 
-#
-#
+if len(sys.argv)>=4:
+    zudge.open(sys.argv[3])
+
 #GAME!!!
-#
-#
 
 def disqualify(p:int):
-    write(f"p{p} \"{zudge.sys.argv[p]}\" is disqualified")
-    write(f"p{3-p} \"{zudge.sys.argv[3-p]}\" wins!")
+    zudge.print(f"p{p} \"{zudge.sys.argv[p]}\" is disqualified")
+    zudge.print(f"p{3-p} \"{zudge.sys.argv[3-p]}\" wins!")
     exit(0)
 
 def judge(p1move:int,p2move:int)->None:
     if p1move==p2move:
-        write("draw!")
+        zudge.print("draw!")
         return None
     if p1move==(p2move+1)%3:
-        write(f"p1 \"{zudge.sys.argv[1]}\" wins!")
+        zudge.print(f"p1 \"{zudge.sys.argv[1]}\" wins!")
         return None
-    write(f"p2 \"{zudge.sys.argv[2]}\" wins!")
+    zudge.print(f"p2 \"{zudge.sys.argv[2]}\" wins!")
     return None
 
 moves = ["R","P","S"]
@@ -48,8 +34,8 @@ for _ in range(10):
     p1move = zudge.read(zudge.p1)
     p2move = zudge.read(zudge.p2)
     if p1move is None and p2move is None:
-        write("both players disqualified")
-        write("draw!")
+        zudge.print("both players disqualified")
+        zudge.print("draw!")
         exit(0)
     if p1move is None:
         disqualify(1)
@@ -58,8 +44,8 @@ for _ in range(10):
     p1illegal = p1move not in moves
     p2illegal = p2move not in moves
     if p1illegal and p2illegal:
-        write("both players disqualified")
-        write("draw!")
+        zudge.print("both players disqualified")
+        zudge.print("draw!")
         exit(0)
     if p1illegal:
         disqualify(1)
